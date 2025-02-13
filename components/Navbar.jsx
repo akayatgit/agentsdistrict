c714783f8b913c796baeffc23974049f7b647ebe
AKAY <ashokofficial55@gmail.com>
2025-02-13 19:59:26 +0530

    Fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,28 +13,21 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      
-
       <h2 className="font-extrabold test-[24px] text-white leading-[30px]">
         Home
       </h2>
-
       <h2 className="font-extrabold test-[24px] text-white leading-[30px]">
         About Us
       </h2>
-
       <h2 className="font-extrabold test-[24px] text-white leading-[30px]">
         Our Agents
       </h2>
-
       <h2 className="font-extrabold test-[24px] text-white leading-[30px]">
         How it Works
       </h2>
-
       <h2 className="font-extrabold test-[24px] text-white leading-[30px]">
         Success Stories
       </h2>
-      
     </div>
   </motion.nav>
 );
